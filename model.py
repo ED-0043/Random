4d9b42662cdcc7dbbec0fa713307b89aa52d58a3
Ezeabasili Edward <81073568+EDwith2ds@users.noreply.github.com>
2024-01-15 11:31:10 +0100

    updated the model and started image processing bit by bit

--- model.py
+++ model.py
@@ -1,16 +1,21 @@
-import keras
+from keras.models import Sequential
+from keras.layers import Dense
+# from keras.
 
-class Model(keras.Model):
+class Predator(Sequential):
     def __init__(self):
-        super().__init__()
-        self.layer1 = keras.layers.Dense(units=100, activation='elu')
-        self.layer2 = keras.layers.Dense(units=100, activation='relu')
-        self.layer3 = keras.layers.Dense(units=100, activation='relu')
-        self.output = keras.layers.Dense(10, activation='softmax')
+        self.layer_one  = Dense(100, input_shape=(4, ), activation='linear')
+        self.layer_two = Dense(75, activation='relu')
+        self.layer_three = Dense(30, activation='relu')
+        self.output = Dense(1)
 
-    def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-
+    def forward(self, value):
+        x = self.layer_one(value)
+        x = self.layer_two(x)
+        x = self.layer_three(x)        
         return self.output(x)
+    
+pred = Predator()
+pred.predict()
+
+# UNDERSTAND HOW TO IMPLEMENT A MODEL WITH KERAS AND PYTORCH
