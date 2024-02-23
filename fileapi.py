bee0422469edc916942af2320fd228234a09b4ef
Ezeabasili Edward <81073568+EDwith2ds@users.noreply.github.com>
2024-02-23 21:02:13 +0100

    clean up

--- fileapi.py
+++ fileapi.py
@@ -1,23 +1,20 @@
-from fastapi import FastAPI, Path
-from pydantic import BaseModel
-from typing import Optional
-from .model import Model
-
-class Item(BaseModel):
-    name: str
-    date: str
-    age: int
-    phone_num: Optional[int]
+from fastapi import FastAPI
+from typing import List
 
 app = FastAPI()
-model = Model()
 
-@app.post(path=Path(description="unknown destination"))
+@app.post()
 def post_item():
-    return {
-        model.forward()
-        }
+    return {}
 
-@app.post(path=Path(description="unkown destination"))
+@app.get()
+def get_item():
+    return {}
+
+@app.post()
 def post_item():
-    return{model.forward}
+    return {}
+
+@app.post()
+def post_item():
+    return {}
