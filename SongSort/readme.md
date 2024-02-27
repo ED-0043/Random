87572316cdfae5ab1bd5979e1fd85aa6e573799a
Ezeabasili Edward <81073568+EDwith2ds@users.noreply.github.com>
2024-02-27 15:11:02 +0100

    added nlp_init

--- SongSort/readme.md
+++
@@ -1,8 +0,0 @@
-#### Song Sort
-
-aim of this project:
-To take a playlist from an account and sort the songs in a given order
-
-** whatever else should be in a documentation should be here **
-
-probably going to make an api/backend/whatever for this stuff too, just dont know how yet.
--- fileapi.py
+++ fileapi.py
@@ -3,18 +3,18 @@ from typing import List
 
 app = FastAPI()
 
-@app.post()
-def post_item():
-    return {}
+@app.get("/items/{items_id}")
+def home(items_id):
+    return {"items_id": items_id}
 
-@app.get()
-def get_item():
-    return {}
+# @app.get()
+# def get_item():
+#     return {}
 
-@app.post()
-def post_item():
-    return {}
+# @app.post()
+# def post_item():
+#     return {}
 
-@app.post()
-def post_item():
-    return {}
+# @app.post()
+# def post_item():
+#     return {}
