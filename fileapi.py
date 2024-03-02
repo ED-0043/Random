8dbcc84ac9db92b436c8c77dbf2a507a866c6610
Ezeabasili Edward <81073568+EDwith2ds@users.noreply.github.com>
2024-03-02 09:07:25 +0100

    created nlp class file and made changes to fileapi.py

--- fileapi.py
+++ fileapi.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from typing import List
+from modelnlp import SentimentAnalyser
 
 app = FastAPI()
+model = SentimentAnalyser()
 
 @app.get("/items/{items_id}")
 def home(items_id):
@@ -9,12 +11,15 @@ def home(items_id):
 
 # @app.get()
 # def get_item():
-#     return {}
+#     return {model.dataframer()}
 
 # @app.post()
 # def post_item():
-#     return {}
+#     return {model.preprocess_text()}
 
 # @app.post()
 # def post_item():
-#     return {}
+#     return {model.return_predictions()}
+
+model.clean_data()
+model.readablility_score()
